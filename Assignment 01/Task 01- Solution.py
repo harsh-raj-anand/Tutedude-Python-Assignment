458fc67e6cbe3641990e4bde60ce332676334c67
Harsh Raj Anand <138981432+harsh-raj-anand@users.noreply.github.com>
2026-02-03 14:36:00 +0530

    Add files via upload

---
+++ Assignment 01/Task 01- Solution.py
@@ -0,0 +1,16 @@
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+
+add = num1 + num2
+sub = num1 - num2
+mul = num1 * num2
+
+if num2 != 0:
+    div = float(num1) / float(num2)
+else:
+    div = "Undefined (division by zero)"
+
+print("Addition:", add)
+print("Subtraction:", sub)
+print("Multiplication:", mul)
+print("Division:", div)
---
+++ Assignment 01/Task 02- Solution.py
@@ -0,0 +1,6 @@
+FirstName = input("Enter your first name: ")
+LastName = input("Enter your last name: ")
+
+FullName = FirstName + " " + LastName
+
+print("Hello,", FullName + "! Welcome to the Python program.")
